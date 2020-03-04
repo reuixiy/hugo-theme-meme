adcc50ab31ce64bba75a7328eecea03440e8a904
reuixiy <reuixiy@gmail.com>
2020-03-05 00:10:43 +0800

    fix: optimize user experience

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-// Wrap `<table>` to make it responsive
-// Reference: https://github.com/stone-zeng/stone-zeng.github.io/blob/master/js/script.js
-
-document.querySelectorAll('table').forEach((e) => {
-    e.outerHTML = `<div class="table-container">${e.outerHTML}</div>`;
-});
